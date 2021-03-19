c0dede4cf424af0528e8c1d02115b6da1880f857
rishi-vakharia <rishi.rvv@gmail.com>
2021-03-19 16:13:46 +0530

    changed href of sign-in on Register page

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100,unique=True)
+    title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    Name = models.CharField(max_length=200)
+    Email = models.EmailField()
+    Body = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.Body
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('all_users/', views.all_users, name='all_users'),
+    path('profile/',views.my_blogs,name='my_blogs'),
+    path('get-user-blogs/<int:pk>/',views.get_user_blogs,name='user_blogs'),
 ]
