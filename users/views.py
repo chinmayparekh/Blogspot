3430338a7b51ffa5800105544aa873e250774491
Rishi Vakharia <rishi.rvv@gmail.com>
2021-03-19 16:47:52 +0530

    added href on sign in

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
 def register(request):
@@ -15,6 +14,3 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
-@login_required
-def profile(request):
-    return render(request, 'users/profile.html', context)
